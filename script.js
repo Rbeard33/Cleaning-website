ab28374f098e29e5262795907def6cef88faeaa9
Rbeard33 <Riley.beard20@gmail.com>
2024-07-17 20:52:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById("quoteModal");
     const spans = document.getElementsByClassName("close")[0];
 
-    document.querySelectorAll('.service-category ul li').forEach(item => {
+    document.querySelectorAll('.service-category ul li, .service-category p').forEach(item => {
         item.addEventListener('click', () => {
             modal.style.display = "block";
         });
