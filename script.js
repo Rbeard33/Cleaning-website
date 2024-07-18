24728fe3b8bcd619743088e0d755c786fd4e0708
Rbeard33 <Riley.beard20@gmail.com>
2024-07-17 19:19:57 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById("quoteModal");
+    const spans = document.getElementsByClassName("close")[0];
+
+    document.querySelectorAll('.service-category ul li').forEach(item => {
+        item.addEventListener('click', () => {
+            modal.style.display = "block";
+        });
+    });
+
+    spans.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
